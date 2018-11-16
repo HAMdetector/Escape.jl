b3ae24f4dbb68641b7af5bf012bf9cc30c27701f
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2018-11-16 14:24:43 +0100

    add tests for run(::AbstractHLAAnalysis, ::String)

--- test/hla_analysis.jl
+++ test/hla_analysis.jl
@@ -3,3 +3,26 @@
     @test HLAAnalysis("BernoulliPhylogeny", BernoulliPhylogenyModel(),
         HLADataset("Rousseau")) isa AbstractHLAAnalysis
 end
+
+@testset "run(::AbstractHLAAnalysis, ::String)" begin
+    fasta_path = joinpath(dirname(@__DIR__), "test",  "data", "test_large.fasta")
+    hla_types = rand(HLAType, 15)
+    hla_types[5] = HLAType((parse_allele("A11"), parse_allele("A13"),
+                            parse_allele("B53"), parse_allele("B56"),
+                            missing, missing))
+    hla_types[6] = HLAType((missing, missing, missing, missing, missing, missing))
+    hla_data_1 = HLAData("protein_1", fasta_path, hla_types)
+    hla_data_2 = HLAData("protein_2", fasta_path, rand(HLAType, 15))
+
+    dataset = HLADataset("Test", [hla_data_1, hla_data_2])
+    analysis = HLAAnalysis("testrun", BernoulliPhylogenyModel(), dataset)
+
+    testrun_dir = joinpath(dirname(@__DIR__), "test", "data")
+
+    rm(joinpath(testrun_dir, "testrun"), force = true, recursive = true)
+    @suppress Escape.run(analysis, testrun_dir)
+    @test isdir(testrun_dir)
+    result = FileIO.load(joinpath(testrun_dir, "testrun", "analysis_result.jld2"), 
+                         "analysis_result")
+    @test result isa HLAAnalysisResult
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
-using Test, Escape, BioSequences, Suppressor, LightGraphs, MetaGraphs, RCall
+using Test, Escape, BioSequences, Suppressor, LightGraphs, MetaGraphs, RCall, JLD2, FileIO
 
-include("alleles.jl")
-include("dataset.jl")
-include("dataset_rousseau.jl")
-include("replacement.jl")
-include("phylogenetic_tree.jl")
-include("rate_matrix.jl")
-include("phylogenetic_background.jl")
-include("bernoulli_model.jl")
-include("bernoulli_phylogeny_model.jl")
+# include("alleles.jl")
+# include("dataset.jl")
+# include("dataset_rousseau.jl")
+# include("replacement.jl")
+# include("phylogenetic_tree.jl")
+# include("rate_matrix.jl")
+# include("phylogenetic_background.jl")
+# include("bernoulli_model.jl")
+# include("bernoulli_phylogeny_model.jl")
 include("hla_analysis.jl")
