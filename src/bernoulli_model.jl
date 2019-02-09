7dc7cf145f1689c2023caf38c75f4dcc91f8bc50
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2019-02-09 17:00:13 +0100

    enforce limit_hla_accuracy, use Dict for allele_posteriors

--- src/bernoulli_model.jl
+++ src/bernoulli_model.jl
@@ -15,7 +15,7 @@ end
 BernoulliModel(; iter = 2000, chains = 4) = BernoulliModel(:finnish_horseshoe, iter, chains)
 
 function run(model::BernoulliModel, data::AbstractHLAData, replacement::Replacement;
-             wp::WorkerPool = WorkerPool())
+             wp::WorkerPool = WorkerPool(), depth::Int = 1)
     if model.prior == :finnish_horseshoe
         path = joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_hs")
     elseif model.prior == :broad_t
@@ -25,14 +25,15 @@ function run(model::BernoulliModel, data::AbstractHLAData, replacement::Replacem
     input = stan_input(model, data, replacement)
     sf = stan(path, input, chains = model.chains, iter = model.iter, wp = wp,
               stan_args = "adapt delta=0.97")
-    alleles = sort(unique_alleles(filter(x -> missing ∉ x, data.hla_types)))
+    alleles = sort(unique_alleles(filter(x -> missing ∉ x, data.hla_types), depth = depth))
 
     return BernoulliResult(sf, alleles, replacement)
 end
 
-function stan_input(model::BernoulliModel, data::AbstractHLAData, replacement::Replacement)
+function stan_input(model::BernoulliModel, data::AbstractHLAData, replacement::Replacement;
+                    depth::Int = 1)
     y = targets(replacement, data)
-    m = hla_matrix(data.hla_types)
+    m = hla_matrix(data.hla_types, depth = depth)
 
     stan_input = Dict("y" => collect(skipmissing(y)), "hla_matrix" => m[.!ismissing.(y), :],
                       "n_entries" => length(collect(skipmissing(y))), 
