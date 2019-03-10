e1da38d83e770f650065ac35d915d1b09f462b5a
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2019-03-10 13:23:04 +0100

    add stan models to build script

--- deps/build.jl
+++ deps/build.jl
@@ -11,3 +11,9 @@ build_binary(joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_phylogeny_hs
 
 build_binary(joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_hs.stan"),
              joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_hs"))
+
+build_binary(joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_narrow_t.stan"),
+             joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_narrow_t"))
+
+build_binary(joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_phylogeny_narrow_t.stan"),
+             joinpath(dirname(@__DIR__), "data", "stan", "bernoulli_phylogeny_narrow_t"))
