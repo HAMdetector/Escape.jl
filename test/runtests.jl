8c4389a67f73dce59800f571b0aa37a3eff58def
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2019-11-28 17:21:27 +0100

    add EpitopeMap struct

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test, Escape, BioSequences, Suppressor, LightGraphs, MetaGraphs, JLD2, Fil
       StanInterface, Distributed, HypothesisTests, Serialization, DataFrames
 
 import LightGraphs.SimpleGraphs
+import Escape.EpitopeMap
 
 include("alleles.jl")
 include("abstract_hla_model.jl")
@@ -13,6 +14,7 @@ include("phylogenetic_tree.jl")
 include("rate_matrix.jl")
 include("phylogenetic_background.jl")
 include("epitope_prediction.jl")
+include("epitope_map.jl")
 include("bernoulli_model.jl")
 include("bernoulli_phylogeny_model.jl")
 include("fisher_test.jl")
