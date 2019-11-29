3024c91b1b16e29a0f5be61abc1298eefa85ef54
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2019-11-29 13:43:39 +0100

    change fieldnames for EpitopeMap, use correct start and stop positions for epitope_map(::AbstractHLAData)

--- test/epitope_map.jl
+++ test/epitope_map.jl
@@ -2,16 +2,19 @@
     epitopes = ["SLYNTVATLY", "LYNTVATL"]
     start = [77, 78]
     stop = [86, 85]
-    @test EpitopeMap("test", epitopes, start, stop, [parse_allele("B51")]) isa EpitopeMap
+    @test EpitopeMap("test", epitopes, start, stop, 
+        [parse_allele("B51"), parse_allele("A01")], 100) isa EpitopeMap
 
     @test_throws ErrorException EpitopeMap("", ["SLYNTVATL", ""], [77], [84], 
-        [parse_allele("A11")]) isa EpitopeMap
+        [parse_allele("A11")], 100) isa EpitopeMap
     @test_throws ErrorException EpitopeMap("", ["SLYNTVATLY"], [85], [77],
-        [parse_allele("A03")]) isa EpitopeMap
+        [parse_allele("A03")], 100) isa EpitopeMap
     @test_throws ErrorException EpitopeMap("", ["SLYNTVATLY"], [-3], [5],
-        [parse_allele("A01")]) isa EpitopeMap
+        [parse_allele("A01")], 100) isa EpitopeMap
     @test_throws ErrorException EpitopeMap("", ["SLYNTVATLY"], [77], [85],
-        [parse_allele("A68")]) isa EpitopeMap
+        [parse_allele("A68")], 100) isa EpitopeMap
+    @test_throws ErrorException EpitopeMap("test", epitopes, start, stop, 
+        [parse_allele("B51")], 40) isa EpitopeMap
 end
 
 @testset "epitope_map(::AbstractHLAData)" begin
@@ -26,4 +29,5 @@ end
     map = Escape.epitope_map(hla_data)
 
     @test map isa Escape.EpitopeMap
+    @test map.maplength == 14
 end
