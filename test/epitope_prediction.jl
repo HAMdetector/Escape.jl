5f3959a29bcbd947eb25201f8d31a39f8f9344ae
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-01-16 16:19:37 +0100

    add epitope_feature_matrix(::AbstractHLAData) test

--- test/epitope_prediction.jl
+++ test/epitope_prediction.jl
@@ -41,3 +41,27 @@ end
 
     @test nrow(df) > 0
 end
+
+@testset "epitope_feature_matrix(::AbstractHLAData)" begin
+    hla_types = [HLAType(parse_allele("A01", "A02", "B05", "B06", "C03", "C07")),
+                 HLAType(parse_allele("A01", "A03", "B03", "B05", "C03", "C07")),
+                 HLAType(parse_allele("A01", "A04", "B03", "B05", "C03", "C07")),
+                 HLAType(parse_allele("A02", "A04", "B03", "B04", "C03", "C07")),
+                 HLAType(parse_allele("A02", "A04", "B03", "B04", "C03", "C07"))]
+
+    fasta_path = joinpath(@__DIR__, "data", "test.fasta")
+    hla_data = HLAData("test", fasta_path, hla_types, missing)
+
+    # predicted epitopes at:
+    # A02: 5-7, A03: 4+6, C*07
+
+    expected = zeros(Escape.fasta_length(fasta_path), length(unique_alleles(hla_types)))
+
+    # A01, A02, A03, A04, B03, B04, B05, B06, C03, C07
+    expected[4, :] = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] # A03 has predicted epitope at pos. 6
+    expected[5, :] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    expected[6, :] = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+    expected[7, :] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    @test_broken Escape.epitope_feature_matrix(hla_types) == expected
+end
