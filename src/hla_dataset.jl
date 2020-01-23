e18b50de3bf2bbddb661866b2c2e98e017db6421
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-01-23 17:34:21 +0100

    add HLADataset(::Val{:Test})

--- src/hla_dataset.jl
+++ src/hla_dataset.jl
@@ -6,3 +6,18 @@ struct HLADataset <: AbstractHLADataset
     name::String
     data::Vector{<: AbstractHLAData}
 end
+
+function Escape.HLADataset(::Val{:Test})
+    fasta_path = joinpath(@__DIR__, "..", "data", "test_sequences", "test_large.fasta")
+    hla_types = rand(HLAType, 15)
+    hla_types[5] = HLAType((parse_allele("A11"), parse_allele("A13"),
+                            parse_allele("B53"), parse_allele("B56"),
+                            missing, missing))
+    hla_types[6] = HLAType((missing, missing, missing, missing, missing, missing))
+    hla_data_1 = HLAData("protein_1", fasta_path, hla_types, missing)
+    hla_data_2 = HLAData("protein_2", fasta_path, rand(HLAType, 15), missing)
+
+    dataset = HLADataset("Test", [hla_data_1, hla_data_2])
+
+    return dataset
+end
