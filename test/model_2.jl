aa40a7c8cc72f378b4021d1d450b9cfb43833796
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-01-24 19:18:26 +0100

    add pointwise_loglikelihoods(::Model3Result)

--- test/model_2.jl
+++ test/model_2.jl
@@ -1,3 +1,16 @@
+@testset "indices(::Model2Result)" begin
+    ds = Escape.HLADataset("Test")
+    res = @suppress Escape.run(
+        Escape.Model2(), ds.data[1], 
+        mincount = 1, iter = 10, warmup = 10, chains = 1
+    )
+
+    @test Escape.indices(res) isa Vector{Tuple{T, T}} where T <: Int
+    @test Escape.indices(res)[1] == (1, 1)
+    @test Escape.indices(res)[end] == (14, 15)
+    @test length(Escape.indices(res)) == 14 * 15
+end
+
 @testset "pointwise_loglikelihoods(::Model2Result, ::Int, ::Int)" begin
     ds = Escape.HLADataset("Test")
     data = Escape.stan_input(Escape.Model2(), ds.data[1], mincount = 2)
@@ -21,17 +34,4 @@
         end
     end
 
-end
-
-@testset "indices(::Model2Result)" begin
-    ds = Escape.HLADataset("Test")
-    res = @suppress Escape.run(
-        Escape.Model2(), ds.data[1], 
-        mincount = 1, iter = 10, warmup = 10, chains = 1
-    )
-
-    @test Escape.indices(res) isa Vector{Tuple{T, T}} where T <: Int
-    @test Escape.indices(res)[1] == (1, 1)
-    @test Escape.indices(res)[end] == (14, 15)
-    @test length(Escape.indices(res)) == 14 * 15
 end
