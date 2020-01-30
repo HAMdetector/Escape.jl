8ece96a45126c49756e4b90caa417f2728887bf9
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-01-30 15:58:44 +0100

    add Escape.loo(::EscapeResult)

--- src/Escape.jl
+++ src/Escape.jl
@@ -4,14 +4,13 @@ using Base.Threads
 using LinearAlgebra, Calculus, Statistics, DelimitedFiles, Distributed, Serialization
 using StanInterface, LightGraphs, MetaGraphs, Suppressor, DataFrames,
       JuMP, Ipopt, HypothesisTests, MultipleTesting, BioSequences,
-      StatsBase, StatsFuns, Distributions, Loo, RecipesBase
+      StatsBase, StatsFuns, Distributions, Loo, RecipesBase, Base.Threads
 
 include("alleles.jl")
 include("abstract_hla_data.jl")
 include("replacement.jl")
 include("abstract_hla_model.jl")
 include("hla_dataset.jl")
-include("loo.jl")
 include("dataset_rousseau.jl")
 include("phylogenetic_tree.jl")
 include("hla_data.jl")
@@ -25,6 +24,7 @@ include("model_3.jl")
 include("model_4.jl")
 include("fisher_test.jl")
 include("escape_result.jl")
+include("loo.jl")
 include("posterior_predictive_checks.jl")
 include("plots.jl")
 
--- src/loo.jl
+++ src/loo.jl
@@ -1,10 +1,23 @@
-function Escape.loo(result::HLAModelResult)
+function Escape.loo(result::EscapeResult)
     pw = Loo.PointwiseLoo[]
-    sizehint!(pw, length(indices(result)))
+    size = Tuple{Int, Int}[]
 
-    for idx in indices(result)
-        ll = pointwise_loglikelihoods(result, idx...)
-        push!(pw, Loo.pointwise_loo(ll))
+    for res in result
+        loo = Escape.loo(res)
+        push!(pw, loo.pointwise_loo...)
+        push!(size, loo.size)
+    end
+
+    return Loo.LooResult(pw, (size[1][1], sum(x -> x[2], size)))
+end
+
+function Escape.loo(result::HLAModelResult)
+    indices_ = indices(result)
+    pw = Vector{Loo.PointwiseLoo}(undef, length(indices_))
+    
+    for i in 1:length(indices_)
+        ll = pointwise_loglikelihoods(result, indices_[i]...)
+        pw[i] = Loo.pointwise_loo(ll)
     end
 
     draws = result.sf.iter * result.sf.chains
