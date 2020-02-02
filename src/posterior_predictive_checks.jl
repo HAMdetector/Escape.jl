42b6d8055c873a8206faaf6866cc46a5dcd03063
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-02-02 19:22:29 +0100

    use correct number of bins in binned_intervals(::AbstractVector{<:Real}, ::AbstractVector{<:Bool})

--- src/posterior_predictive_checks.jl
+++ src/posterior_predictive_checks.jl
@@ -113,7 +113,7 @@ end
 end
 
 function binned_intervals(
-    theta::AbstractVector{<: Real}, y::AbstractVector{<: Bool};
+    theta::AbstractVector{<:Real}, y::AbstractVector{<:Bool};
     bins::Int = 10, lower::Real = 0.025, upper = 0.975
 )   
     check_calibration_arguments(theta, y)
@@ -127,14 +127,17 @@ function binned_intervals(
         lower = Float64[], upper = Float64[]
     )
 
-    for partition in Iterators.partition(1:length(theta), div(length(theta), bins))
+    binsize = div(length(theta), bins)
+    for (i, partition) in enumerate(Iterators.partition(1:length(theta), binsize))
         y_p = y[partition]
         theta_p = theta[partition]
         d = Beta(sum(y_p) + 1, length(y_p) - sum(y_p) + 1)
 
         interval = invlogcdf.(d, (log(lower), log(upper)))
 
-        push!(df, (mean(theta_p), mean(y_p), interval...))
+        if i <= bins
+            push!(df, (mean(theta_p), mean(y_p), interval...))
+        end
     end
 
     return df
