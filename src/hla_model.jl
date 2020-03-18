e114e8d05d3d119b5f847d99f29a72d30dc636c3
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2020-03-18 22:28:03 +0100

    do not use undef for matrix initialization for stan data

--- src/hla_model.jl
+++ src/hla_model.jl
@@ -74,6 +74,7 @@ function ys_maprect(
     R = length(r)
     N = length(data.hla_types)
     ys =  Matrix{Int}(undef, R, N + 1)
+    fill!(ys, -1)
 
     for i in 1:R
         t = targets(r[i], data)
