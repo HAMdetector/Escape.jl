b11ccda39f9b429d967e32c206cb6ad6a41b8349
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2021-04-06 03:16:25 +0200

    fix issue in build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 using StanInterface
 
-for i in (1, 4)
-    path = joinpath(@__DIR__, "..", "data", "stan", "model_$(i).stan")
+for i in 1:4
+    path = joinpath(@__DIR__, "..", "models", "model_$(i).stan")
     build_binary(path)
 end
