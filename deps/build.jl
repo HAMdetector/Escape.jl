16fdad3286797e6c98ef148b70b76251c5ea2cbe
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2021-04-17 01:10:46 +0200

    fix broken build script

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,5 @@ using StanInterface
 
 for i in 1:4
     path = joinpath(@__DIR__, "..", "models", "model_$(i).stan")
-    isfile(path) || build_binary(path)
+    isfile(joinpath(@__DIR__, "..", "models", "model_$i")) || build_binary(path)
 end
