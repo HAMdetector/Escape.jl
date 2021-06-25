5f6425ef390af395c16beb3ce6f33ed02466ecb5
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2021-06-25 02:35:10 +0200

    fix split_hla_data(::HLAData) to work for situations where the stan_input field is missing

--- src/split_hla_data.jl
+++ src/split_hla_data.jl
@@ -42,25 +42,27 @@ end
 function split_hla_data(data::HLAData, n::Int = 10)
     split_data = SplitHLAData[]
     replacements = Escape.replacements(data, mincount = 1)
-    R = data.stan_input["R"]
+    stan_input = Escape.stan_input(HLAModel{4}(), data)
+    R = stan_input["R"]
     splits = Iterators.partition(1:R, ceil(Int, R // n))
 
     for (i, split) in collect(enumerate(splits))
-        stan_input = copy(Escape.stan_input(data))
-        in_split = findall(x -> x in split, stan_input["rs"])
+        stan_input_split = copy(stan_input)
+        in_split = findall(x -> x in split, stan_input_split["rs"])
 
-        stan_input["Z"] = stan_input["Z"][split, :]
-        stan_input["rs"] = map(x -> findfirst(y -> y == x, split), stan_input["rs"][in_split])
-        stan_input["idx"] = stan_input["idx"][in_split]
-        stan_input["y"] = stan_input["y"][in_split]
-        stan_input["N"] = length(in_split)
-        stan_input["R"] = length(split)
-        stan_input["phy"] = stan_input["phy"][split, :]
+        stan_input_split["Z"] = stan_input_split["Z"][split, :]
+        stan_input_split["rs"] = map(x -> findfirst(y -> y == x, split), 
+            stan_input_split["rs"][in_split])
+        stan_input_split["idx"] = stan_input_split["idx"][in_split]
+        stan_input_split["y"] = stan_input_split["y"][in_split]
+        stan_input_split["N"] = length(in_split)
+        stan_input_split["R"] = length(split)
+        stan_input_split["phy"] = stan_input_split["phy"][split, :]
         
         split_data_ = SplitHLAData(data.name, data.records, data.hla_types, data.tree,
-            stan_input, data, split, i)
+            stan_input_split, data, split, i)
         
-        @assert length(Escape.replacements(split_data_)) == stan_input["R"]
+        @assert length(Escape.replacements(split_data_)) == stan_input_split["R"]
         
         push!(split_data, split_data_)
     end
