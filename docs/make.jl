078fbf010a24b37757be7f73c765d5a12b833e28
Daniel Habermann <10801656+HAMdetector@users.noreply.github.com>
2021-07-01 21:12:39 +0200

    update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,15 @@ using Escape
 
 makedocs(
     sitename = "Escape.jl",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(
+        canonical="https://HAMdetector.github.io/MyPkg2.jl",
+    ),
     modules = [Escape],
+    repo = "github.com/HAMdetector/Escape.jl.git",
     pages = [
         "quick_start.md",
         "advanced_usage.md"
-    ]
+    ],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
